e6cdc06d571ab897480af00590c786d48530c964
dilshod222 <78033018+dilshod222@users.noreply.github.com>
2021-10-25 22:33:29 +0500

    o'zgardi

--- project/models.py
+++ project/models.py
@@ -1,7 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
-from utils.utils import Auditable
 # Create your models here.
 
+class Auditable(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    is_deleted = models.IntegerField(default=0)
+    class Meta:
+        abstract = True
 
 class Project(Auditable):
     name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    doc_path = models.CharField(max_length=200)
+    closed = models.BooleanField()
+    estimated_deadline = models.DateTimeField(null=True)
+    class Meta:
+        db_table='project'
+
+
+class ProjectColumns(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    class Meta:
+        db_table='project_columns'
+
--- task/models.py
+++ task/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from utils.utils import Auditable
+
+from project.models import ProjectColumns, Auditable
 
 
 # Create your models here.
@@ -8,3 +9,6 @@ from utils.utils import Auditable
 class Task(Auditable):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    column_id=models.ForeignKey(ProjectColumns, on_delete=models.CASCADE)
+    class Meta:
+        db_table='task'
