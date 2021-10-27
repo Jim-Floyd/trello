a295e13a277024097434a96e908de53337292af6
dilshod222 <78033018+dilshod222@users.noreply.github.com>
2021-10-27 21:35:46 +0500

    Diagramma tuzildi

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from authentication.views import UserCreate, UserLogin
+from django.contrib.auth.views import LogoutView
 
 app_name = 'authentication'
 
 
 urlpatterns = [
     path('register/', UserCreate.as_view(), name = 'register'),
-    path('login/', UserLogin.as_view(), name = 'login')
+    path('login/', UserLogin.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(), name = 'logout')
 ]
